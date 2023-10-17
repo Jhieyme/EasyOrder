24445dced9f78c93fb09df4e0b0928c3938f96f3
jennifer <jennifertorrespalomino@gmail.com>
2023-10-17 04:23:51 -0500

    actualizacion

--- app/src/main/java/com/jennifer/easyorder/Adapter/CustomerAdapter.java
+++ app/src/main/java/com/jennifer/easyorder/Adapter/CustomerAdapter.java
@@ -18,34 +18,34 @@ public class CustomerAdapter extends RecyclerView.Adapter<CustomerAdapter.ShowVi
     this.customers = customers;
   }
 
-  @NonNull
-  @Override
-  public ShowViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-    ItemCustomerBinding binding = ItemCustomerBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
-    return new ShowViewHolder(binding);
-  }
-
-  @Override
-  public void onBindViewHolder(@NonNull ShowViewHolder holder, int position) {
-    holder.bind(customers.get(position));
-  }
-
-  @Override
-  public int getItemCount() {
-    return customers.size();
-  }
-
-  public class ShowViewHolder extends RecyclerView.ViewHolder {
-    private ItemCustomerBinding binding;
-    public ShowViewHolder(@NonNull ItemCustomerBinding binding) {
-      super(binding.getRoot());
-      this.binding = binding;
+    @NonNull
+    @Override
+    public ShowViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+      ItemCustomerBinding binding = ItemCustomerBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
+      return new ShowViewHolder(binding);
     }
 
-    public void bind(Customer customer) {
-      binding.txtName.setText(customer.getNombres());
-//      binding.txtDNI.setText(customer.getDni());
+    @Override
+    public void onBindViewHolder(@NonNull ShowViewHolder holder, int position) {
+      holder.bind(customers.get(position));
     }
 
-  }
+    @Override
+    public int getItemCount() {
+      return customers.size();
+    }
+
+    public class ShowViewHolder extends RecyclerView.ViewHolder {
+        private ItemCustomerBinding binding;
+        public ShowViewHolder(@NonNull ItemCustomerBinding binding) {
+          super(binding.getRoot());
+          this.binding = binding;
+        }
+
+        public void bind(Customer customer) {
+          binding.txtName.setText(customer.getNombres());
+          binding.txtLastname.setText(customer.getApellidos());
+          binding.txtDni.setText(customer.getDni());
+        }
+    }
 }
