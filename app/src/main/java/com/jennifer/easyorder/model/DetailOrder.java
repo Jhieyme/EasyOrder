9afb9c39f9e353a5527ae1aabb206639bf9a9625
jennifer <jennifertorrespalomino@gmail.com>
2023-10-17 04:02:45 -0500

    actualizacion

---
+++ app/src/main/java/com/jennifer/easyorder/model/DetailOrder.java
@@ -0,0 +1,12 @@
+package com.jennifer.easyorder.model;
+
+public class DetailOrder {
+
+    private int idDetalleComanda;
+    private int cantidad;
+    private double subTotal;
+    private double total;
+
+    private Customer customer;
+
+}
--- app/src/main/java/com/jennifer/easyorder/model/Order.java
+++ app/src/main/java/com/jennifer/easyorder/model/Order.java
@@ -1,4 +1,11 @@
 package com.jennifer.easyorder.model;
 
+import java.util.Date;
+
 public class Order {
+
+    private int idComanda;
+    private Date fechaHora;
+    private String estado;
+
 }
