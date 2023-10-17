0ebd43701eafe714a3eea0e0f52d8ae42967c521
Diego <SM76496558@idat.edu.pe>
2023-10-17 01:31:07 -0500

    qlq chama

--- app/src/main/java/com/jennifer/easyorder/Adapter/CustomerAdapter.java
+++ app/src/main/java/com/jennifer/easyorder/Adapter/CustomerAdapter.java
@@ -1,4 +1,52 @@
 package com.jennifer.easyorder.Adapter;
 
-public class CustomerAdapter {
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.jennifer.easyorder.databinding.ItemCustomerBinding;
+import com.jennifer.easyorder.model.Customer;
+
+
+import java.util.List;
+
+public class CustomerAdapter extends RecyclerView.Adapter<CustomerAdapter.ShowViewHolder> {
+  private List<Customer> customers;
+  public CustomerAdapter(List<Customer> customers) {
+    this.customers = customers;
+  }
+
+  @NonNull
+  @Override
+  public CustomerAdapter.ShowViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    ItemCustomerBinding binding = ItemCustomerBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
+    return new CustomerAdapter.ShowViewHolder(binding);
+  }
+
+  @Override
+  public void onBindViewHolder(@NonNull CustomerAdapter.ShowViewHolder holder, int position) {
+    holder.bind(customers.get(position));
+  }
+
+  @Override
+  public int getItemCount() {
+    return customers.size();
+  }
+
+  public class ShowViewHolder extends RecyclerView.ViewHolder {
+    private ItemCustomerBinding binding;
+    public ShowViewHolder(@NonNull ItemCustomerBinding binding) {
+      super(binding.getRoot());
+      this.binding = binding;
+    }
+
+    public void bind(Customer customer) {
+      binding.txtName.setText("Diego Armando Torre Macetas");
+      binding.txtDNI.setText("76496558");
+    }
+
+  }
 }
--- app/src/main/java/com/jennifer/easyorder/Fragments/CustomerFragment.java
+++ app/src/main/java/com/jennifer/easyorder/Fragments/CustomerFragment.java
@@ -2,19 +2,69 @@ package com.jennifer.easyorder.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.jennifer.easyorder.Adapter.CategoryAdapter;
+import com.jennifer.easyorder.Adapter.CustomerAdapter;
 import com.jennifer.easyorder.R;
+import com.jennifer.easyorder.data.RestaurantInterface;
+import com.jennifer.easyorder.data.RetrofitHelper;
+import com.jennifer.easyorder.databinding.FragmentCategoryBinding;
+import com.jennifer.easyorder.databinding.FragmentCustomerBinding;
+import com.jennifer.easyorder.model.Category;
+import com.jennifer.easyorder.model.Customer;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class CustomerFragment extends Fragment {
+
+    private FragmentCustomerBinding binding;
+    private RecyclerView recyclerView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_customer, container, false);
+        binding = FragmentCustomerBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        recyclerView = view.findViewById(R.id.rv_customer);
+        GridLayoutManager layoutManager = new GridLayoutManager(view.getContext(), 2);
+        binding.rvCustomer.setLayoutManager(layoutManager);
+
+        RestaurantInterface customerInterface = RetrofitHelper.getInstance().create(RestaurantInterface.class);
+        Call<List<Customer>> call = customerInterface.getShowCustomer();
+
+        call.enqueue(new Callback<List<Customer>>() {
+
+            @Override
+            public void onResponse(Call<List<Customer>> call, Response<List<Customer>> response) {
+                List<Customer> items = response.body();
+                CustomerAdapter rvCustomerAdapter = new CustomerAdapter(items);
+                recyclerView.setAdapter(rvCustomerAdapter);
+            }
+
+            @Override
+            public void onFailure(Call<List<Customer>> call, Throwable t) {
+
+            }
+        });
     }
 }
