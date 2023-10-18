e955a6c232af4e7e938c04651d3b52b26f0aabc4
Diego <SM76496558@idat.edu.pe>
2023-10-18 14:18:42 -0500

    ProductAdapter/ItemProduct2 update

--- app/src/main/java/com/jennifer/easyorder/Adapter/ProductAdapter.java
+++ app/src/main/java/com/jennifer/easyorder/Adapter/ProductAdapter.java
@@ -1,7 +1,9 @@
 package com.jennifer.easyorder.Adapter;
 
+import android.annotation.SuppressLint;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,42 +14,75 @@ import com.jennifer.easyorder.model.Product;
 
 import java.util.List;
 
-public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ShowViewHolder>{
+public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ShowViewHolder> {
 
-    private List<Product> products;
-    public ProductAdapter(List<Product> products) {
-        this.products = products;
+
+  private List<Product> products;
+
+  public ProductAdapter(List<Product> products) {
+    this.products = products;
+
+  }
+
+  @NonNull
+  @Override
+  public ProductAdapter.ShowViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    ItemProduct2Binding binding = ItemProduct2Binding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
+    return new ProductAdapter.ShowViewHolder(binding);
+  }
+
+  @SuppressLint("ResourceType")
+  @Override
+  public void onBindViewHolder(@NonNull ProductAdapter.ShowViewHolder holder, int position) {
+    holder.bind(products.get(position));
+
+    holder.binding.imgProduct2.setOnClickListener(view -> {
+      Toast.makeText(view.getContext(), "¡Seleccionaste la imagen", Toast.LENGTH_SHORT).show();
+
+    });
+    holder.binding.btnAdd.setOnClickListener(view -> {
+      String currentQuantity = (String) holder.binding.txtCnt.getText().toString();
+      int newQnt = Integer.parseInt(currentQuantity) + 1;
+      if (newQnt > 10) {
+        Toast.makeText(view.getContext(), "¡No puedes seleccionar más de 10 platillos", Toast.LENGTH_SHORT).show();
+      } else {
+        holder.binding.txtCnt.setText(String.valueOf(newQnt));
+      }
+
+
+    });
+    holder.binding.btnMinus.setOnClickListener(view -> {
+      String currentQuantity = (String) holder.binding.txtCnt.getText().toString();
+      int newQnt = Integer.parseInt(currentQuantity) - 1;
+      if (newQnt < 0) {
+        holder.binding.txtCnt.setText(String.valueOf(0));
+      } else {
+        holder.binding.txtCnt.setText(String.valueOf(newQnt));
+      }
+
+    });
+
+
+  }
+
+  @Override
+  public int getItemCount() {
+    return products.size();
+  }
+
+  public class ShowViewHolder extends RecyclerView.ViewHolder {
+    private ItemProduct2Binding binding;
+
+    public ShowViewHolder(@NonNull ItemProduct2Binding binding) {
+      super(binding.getRoot());
+      this.binding = binding;
     }
 
-    @NonNull
-    @Override
-    public ProductAdapter.ShowViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        ItemProduct2Binding binding = ItemProduct2Binding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
-        return new ProductAdapter.ShowViewHolder(binding);
+    public void bind(Product product) {
+      binding.txtName.setText(product.getNombre());
+      binding.txtDescription.setText(product.getDescripcion());
+      binding.txtPrecio.setText(String.format("S/. %.2f", product.getPrecio()));
+      Glide.with(itemView.getContext()).load(product.getUrlImagen()).into(binding.imgProduct2);
     }
-
-    @Override
-    public void onBindViewHolder(@NonNull ProductAdapter.ShowViewHolder holder, int position) {
-        holder.bind(products.get(position));
-    }
-
-    @Override
-    public int getItemCount() {
-        return products.size();
-    }
-
-    public class ShowViewHolder extends RecyclerView.ViewHolder {
-        private ItemProduct2Binding binding;
-        public ShowViewHolder(@NonNull ItemProduct2Binding binding) {
-            super(binding.getRoot());
-            this.binding = binding;
-        }
-
-        public void bind(Product product) {
-            binding.txtName.setText(product.getNombre());
-            binding.txtDescription.setText(product.getDescripcion());
-            binding.txtPrecio.setText(String.format("S/. %.2f", product.getPrecio()));
-            Glide.with(itemView.getContext()).load(product.getUrlImagen()).into(binding.imgProduct2);
-        }
-    }
+  }
 }
